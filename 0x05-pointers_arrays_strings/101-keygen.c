2433df74e6d1907e9e67e28f632ce785ff045b1e
ahmaaaT <a7mdismail24@gmail.com>
2023-09-18 18:09:56 -0700

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+char password[PASSWORD_LENGTH + 1]; /* +1 for the null terminator */
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int i;
 int index;
 
-// Seed the random number generator with the current time
+/* Seed the random number generator with the current time */
 srand(time(NULL));
 
-// Generate a random password
+/* Generate a random password */
 for (i = 0; i < PASSWORD_LENGTH; i++)
 {
 	index = rand() % (sizeof(charset) - 1);
@@ -22,9 +22,8 @@ for (i = 0; i < PASSWORD_LENGTH; i++)
 }
 password[PASSWORD_LENGTH] = '\0';
 
-// Print the generated password
+/* Print the generated password */
 printf("%s\n", password);
 
-return 0;
+return (0);
 }
-
